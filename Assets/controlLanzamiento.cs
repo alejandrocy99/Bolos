d9a2eb4c91c96c90d957241c69d000d53b64e7c6
alejandrocy99 <moflycy@gmail.com>
2025-01-20 10:14:08 +0100

    actualizacion bolos

--- Assets/controlLanzamiento.cs
+++ Assets/controlLanzamiento.cs
@@ -9,35 +9,36 @@ public class controlLanzamiento : MonoBehaviour
     private Camera camara;
     public GameObject bola;
     private Rigidbody2D bolaRB ;
-
+    //public GameObject pointZone;
     private SpringJoint2D bolaSpringJoint;
     public Rigidbody2D pivote;
     private bool estaArrastrando;
+    private  int puntuacion;
     // Start is called before the first frame update
     void Start()
     {
         camara = Camera.main;
+        bola = GameObject.Find("Bola");
         bolaRB = bola.GetComponent<Rigidbody2D>();
         bolaSpringJoint = bola.GetComponent<SpringJoint2D>();
         bolaSpringJoint.connectedBody = pivote;
         estaArrastrando = false;
+        //pointZone = GameObject.FindWithTag("pointzone");
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("puntuacion : " + puntuacion);
+
         if(bolaRB == null) return;
         //si no tocamos la pantalla
-        if(!Touchscreen.current.primaryTouch.press.isPressed && !estaArrastrando){
-            return;
-        }
-        else if(!Touchscreen.current.primaryTouch.press.isPressed && estaArrastrando){
+        if(!Touchscreen.current.primaryTouch.press.isPressed ){
             estaArrastrando = false;
             LanzaBola();
-        
         }
-
         if(Touchscreen.current.primaryTouch.press.isPressed ){
             estaArrastrando = true;
             bolaRB.isKinematic = true;
@@ -47,25 +48,34 @@ public class controlLanzamiento : MonoBehaviour
             bola.transform.position = d; 
             LanzaBola();
         }
-
-
-        
-        
     
-
-        Debug.Log("presionado");
-        //¿donde se a pulsado?
-        Vector2 posicion = Touchscreen.current.primaryTouch.position.ReadValue();
-        Debug.Log("posicion" + posicion);  
-        Vector3 posicionGlobal = camara.ScreenToWorldPoint(posicion); 
-         //screentoworlspoint
     }
 
 
     public void LanzaBola(){
-        bolaRB.isKinematic = false;
-        //if(bola.transform.position.x > pivote.transform.position.x){
+        if(estaArrastrando == false) {
+            bolaRB.isKinematic = false;
+        if(bola.transform.position.x > pivote.transform.position.x){
             bolaSpringJoint.enabled = false;
-        //}
+        }else{
+            bolaSpringJoint.enabled = true;
+        }
+        
+    }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "bolo"){
+            Debug.Log("entro en la zona");
+            puntuacion ++;
+
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other){
+        if(other.gameObject.tag == "bolo"){
+            Debug.Log("salio de la zona");
+            puntuacion --;
+        }
     }
 }
